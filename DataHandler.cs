3f6fbdce7abfec70fe54c9bb56223a0e96df295b
yandrebez <125827114+yandrebez@users.noreply.github.com>
2023-08-26 09:48:45 +0200

    Add files via upload

---
+++ DataHandler.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VaccinationRegistration
+{
+    internal class DataHandler
+    {
+        SqlConnection connection;
+        SqlCommand command;
+        SqlDataReader dtreader;
+
+        string connectionString = "Data Source=(local); Initial Catalog=VacinationRegistrationDB; Integrated Security = SSPI";
+
+        public void registerPatient(int _patientID, string _name, string _surname, int _phonenumber, string _address)
+        {
+            string qryRegister = ("INSERT INTO tblPatientsRegistered VALUES('" + _patientID + "','" + _name + "','" + _surname + "','" + _phonenumber + "','" + _address + "')");
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            command = new SqlCommand(qryRegister, connection);
+
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Patient Registered!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Patient was not registered..." + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public void updatePatient(int _patientID, string _name, string _surname, int _phonenumber, string _address)
+        {
+            string qryUpdate = ("UPDATE tblPatientRegistered SET PatientID = '" + _patientID + "', Name='" + _name + "', Surname= '" + _surname + "', PhoneNumber='" + _phonenumber + "', Address='" + _address + "')");
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            command = new SqlCommand(qryUpdate, connection);
+
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Patient Details UPDATED!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Update was unsuccesfull " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public void deletePatient(int _patientID)
+        {
+            string qryDelete = ("DELETE FROM tblPatientRegistered WHERE PatientID = '" + _patientID + "'");
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            command = new SqlCommand(qryDelete, connection);
+
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Patient has been Deleted!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Could not Delete File..." + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public void searchPatient(int _patientID)
+        {
+            string qrySearch = ("SELECT * FROM tblPatientRegistered WHERE PatientID = '" + _patientID + "'");
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            command = new SqlCommand(qrySearch, connection);
+
+            try
+            {
+                dtreader = command.ExecuteReader();
+                if (dtreader.Read())
+                {
+                    _patientID = int.Parse(dtreader[0].ToString());
+
+                }
+                else
+                {
+                    MessageBox.Show("No patient record found!");
+                }
+                dtreader.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("No record found" + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+       
+    }
+}
+
+    
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VaccinationRegistration
+{
+    public partial class Form1 : Form
+    {
+        DataHandler dtHandler = new DataHandler();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            label10.Visible = true;
+            label11.Visible = true;
+            label12.Visible = true;
+            label13.Visible = true;
+
+            txtShowTelephone.Visible = true;
+            txtShowName.Visible = true;
+            txtShowSurname.Visible = true;
+            txtShowAddress.Visible = true;
+
+            dtHandler.searchPatient(int.Parse(txtSearchPID.Text));
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            dtHandler.registerPatient(int.Parse(txtPatientID.Text), txtName.Text, txtSurname.Text, int.Parse(txtNumber.Text), txtAddress.Text);
+        }
+
+
+        private void dgvVaccinationRegistrations_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            label10.Visible = true;
+            label11.Visible = true;
+            label12.Visible = true;
+            label13.Visible = true;
+
+            txtShowTelephone.Visible = true;
+            txtShowName.Visible = true;
+            txtShowSurname.Visible = true;
+            txtShowAddress.Visible = true;
+
+            if (dgvVaccinationRegistrations.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+            {
+                string patientID = dgvVaccinationRegistrations.Rows[e.RowIndex].Cells["PatientID"].FormattedValue.ToString();
+                string Name = dgvVaccinationRegistrations.Rows[e.RowIndex].Cells["Name"].FormattedValue.ToString();
+                string Surname = dgvVaccinationRegistrations.Rows[e.RowIndex].Cells["Surname"].FormattedValue.ToString();
+                string PhoneNumber = dgvVaccinationRegistrations.Rows[e.RowIndex].Cells["PhoneNumber"].FormattedValue.ToString();
+                string Address = dgvVaccinationRegistrations.Rows[e.RowIndex].Cells["Address"].FormattedValue.ToString();
+
+                txtSearchPID.Text = patientID;
+                txtShowName.Text = Name;
+                txtShowSurname.Text = Surname;
+                txtShowTelephone.Text = PhoneNumber;
+                txtShowAddress.Text = Address;
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            dtHandler.updatePatient(int.Parse(txtSearchPID.Text), txtShowName.Text, txtShowSurname.Text, int.Parse(txtShowTelephone.Text), txtShowAddress.Text);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            dtHandler.deletePatient(int.Parse(txtSearchPID.Text));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtShowTelephone.Visible = false;
+            txtShowName.Visible = false;
+            txtShowSurname.Visible = false;
+            txtShowAddress.Visible = false;
+
+            label10.Visible = false;
+            label11.Visible = false;
+            label12.Visible = false;
+            label13.Visible = false;
+        }
+    }
+}
